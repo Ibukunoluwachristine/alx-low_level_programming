236ad55c1b8c3426a4d419debedfad1e7447171a
Ibukunoluwachristine <onabajochristine@gmail.com>
2022-07-05 07:10:55 -0700

    commit corrected

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,9 @@
-#include "main.h"
-
-
+#include 'main.h'
 
 /** main - prints putchar
 * Return: always 0
 */
-
 int main(void)
-
 {
 	_putchar('_');
 	_putchar('p');
@@ -17,7 +13,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n');
+	_putchar('/n');
 
-return (0);
-}
+	return (0);
