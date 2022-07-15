51449d360916017da5bb824616664b8e76ac71f2
Ibukunoluwachristine <onabajochristine@gmail.com>
2022-07-15 01:27:01 -0700

    hot fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
 *_strncpy - copies a string
-*@dest: A pointer to a character that will be changed
-*@src: A pointer to a character that will be changed
-*@n: value
-*Return: dest
-*/
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: value
+ *Return: dest
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 
